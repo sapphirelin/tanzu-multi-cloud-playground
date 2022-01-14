42b8092f3b8b286cb6da65f2c758a2e3139f86e2
Sapphire Lin <30453370+sapphirelin@users.noreply.github.com>
2022-01-14 16:52:21 +0800

    Create install.sh

---
+++ carvel/install.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+if test -z "$BASH_VERSION"; then
+  echo "Please run this script using bash, not sh or any other shell." >&2
+  exit 1
+fi
+
+install() {
+  set -euo pipefail
+
+  dst_dir="${K14SIO_INSTALL_BIN_DIR:-/usr/local/bin}"
+
+  if [ -x "$(command -v wget)" ]; then
+    dl_bin="wget -nv -O-"
+  else
+    dl_bin="curl -s -L"
+  fi
+
+  shasum -v 1>/dev/null 2>&1 || (echo "Missing shasum binary" && exit 1)
+
+  ytt_version=v0.38.0
+  kbld_version=v0.32.0
+  kapp_version=v0.44.0
+  kwt_version=v0.0.6
+  imgpkg_version=v0.24.0
+  vendir_version=v0.23.0
+
+  if [[ `uname` == Darwin ]]; then
+    binary_type=darwin-amd64
+    ytt_checksum=1f84e26457a5efd2470df408072bf2f182c939414e4958f82e19e122d2ee120c
+    kbld_checksum=5fc8a491327294717611974c6ab3da2bda3f3809ef3147c1e8472ac62af3ee18
+    kapp_checksum=27980ca316c2124fb2e983ff971c9afb12be818ac93d7d63c6b03ddb42d38f97
+    kwt_checksum=555d50d5bed601c2e91f7444b3f44fdc424d721d7da72955725a97f3860e2517
+    imgpkg_checksum=f0c87c8caefb3d2a82e648779b36783403fe5c93930df2d5cbf4968713933392
+    vendir_checksum=aef3233128727c01ffac6561533c9f60a49307f41decc1baa18688a4a250b15f
+  else
+    binary_type=linux-amd64
+    ytt_checksum=2ca800c561464e0b252e5ee5cacff6aa53831e65e2fb9a09cf388d764013c40d
+    kbld_checksum=de546ac46599e981c20ad74cd2deedf2b0f52458885d00b46b759eddb917351a
+    kapp_checksum=aaa5162b857868e64a270641b79a2a980e4528d40e88ea7741440b3028637c7c
+    kwt_checksum=92a1f18be6a8dca15b7537f4cc666713b556630c20c9246b335931a9379196a0
+    imgpkg_checksum=cfcfcb5afc5e3d28ce1f2f67971a4dcd18f514dadf8a63d70c864e49c9ddca7e
+    vendir_checksum=c6a65e7e8e589e25bf5554e9575ab247e55ae71920d3d366ffd84b1571fe34ac
+  fi
+
+  echo "Installing ${binary_type} binaries..."
+
+  echo "Installing ytt..."
+  $dl_bin https://github.com/vmware-tanzu/carvel-ytt/releases/download/${ytt_version}/ytt-${binary_type} > /tmp/ytt
+  echo "${ytt_checksum}  /tmp/ytt" | shasum -c -
+  mv /tmp/ytt ${dst_dir}/ytt
+  chmod +x ${dst_dir}/ytt
+  echo "Installed ${dst_dir}/ytt ${ytt_version}"
+
+  echo "Installing kbld..."
+  $dl_bin https://github.com/vmware-tanzu/carvel-kbld/releases/download/${kbld_version}/kbld-${binary_type} > /tmp/kbld
+  echo "${kbld_checksum}  /tmp/kbld" | shasum -c -
+  mv /tmp/kbld ${dst_dir}/kbld
+  chmod +x ${dst_dir}/kbld
+  echo "Installed ${dst_dir}/kbld ${kbld_version}"
+
+  echo "Installing kapp..."
+  $dl_bin https://github.com/vmware-tanzu/carvel-kapp/releases/download/${kapp_version}/kapp-${binary_type} > /tmp/kapp
+  echo "${kapp_checksum}  /tmp/kapp" | shasum -c -
+  mv /tmp/kapp ${dst_dir}/kapp
+  chmod +x ${dst_dir}/kapp
+  echo "Installed ${dst_dir}/kapp ${kapp_version}"
+
+  echo "Installing kwt..."
+  $dl_bin https://github.com/vmware-tanzu/carvel-kwt/releases/download/${kwt_version}/kwt-${binary_type} > /tmp/kwt
+  echo "${kwt_checksum}  /tmp/kwt" | shasum -c -
+  mv /tmp/kwt ${dst_dir}/kwt
+  chmod +x ${dst_dir}/kwt
+  echo "Installed ${dst_dir}/kwt ${kwt_version}"
+
+  echo "Installing imgpkg..."
+  $dl_bin https://github.com/vmware-tanzu/carvel-imgpkg/releases/download/${imgpkg_version}/imgpkg-${binary_type} > /tmp/imgpkg
+  echo "${imgpkg_checksum}  /tmp/imgpkg" | shasum -c -
+  mv /tmp/imgpkg ${dst_dir}/imgpkg
+  chmod +x ${dst_dir}/imgpkg
+  echo "Installed ${dst_dir}/imgpkg ${imgpkg_version}"
+
+  echo "Installing vendir..."
+  $dl_bin https://github.com/vmware-tanzu/carvel-vendir/releases/download/${vendir_version}/vendir-${binary_type} > /tmp/vendir
+  echo "${vendir_checksum}  /tmp/vendir" | shasum -c -
+  mv /tmp/vendir ${dst_dir}/vendir
+  chmod +x ${dst_dir}/vendir
+  echo "Installed ${dst_dir}/vendir ${vendir_version}"
+}
+
+install
+
+
+
+
+
+
